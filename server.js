90c5c993fea8374e650ed2e796469f2513c8015c
shriram venugopal <shriramvenugopal123@gmail.com>
2018-02-13 15:28:19 +0530

    modified:   server.js 	new file:   ui/article-one.html 	new file:   ui/article-three.html 	new file:   ui/article-two.html 	modified:   ui/index.html

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(_dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(_dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(_dirname, 'ui', 'article-three.html'));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
