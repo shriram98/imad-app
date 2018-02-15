075e9661e83893291dec64103d6a217bd2633c4d
Shriram Venugopal <hawkeye@debian>
2018-02-15 18:09:57 +0530

    applied javascripts properties

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+var element = document.getElementById("main-text");
+element.innerHTML = "This is the new Main text";
+
+var img = document.getElementById("img");
+var marginLeft = 0;
+function moveRight() {
+  marginLeft = marginLeft + 1;
+  img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function() {
+  var interval = setInterval(moveRight, 50);
+};
